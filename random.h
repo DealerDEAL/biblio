714a36429fcee7525bf10e2f45ed637ce5b44c68
DealerDEAL <112918405+DealerDEAL@users.noreply.github.com>
2023-11-21 14:02:39 +0700

    Create random.h

---
+++ random.h
@@ -0,0 +1,11 @@
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+unsigned long seed = 1;  // это то самое стартовое значение,
+                         // о котором мы говорили ранее
+
+int my_rand() {
+  seed = seed * 1103515245 + 12345;  // Линейный конгруэнтный метод
+  return (seed / 65536) % 10;     // Вернуть значение в диапазоне 0-32767
+}
