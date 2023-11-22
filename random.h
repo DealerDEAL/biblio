7834840a2ee26cbf4867f8bb87173f4f00ea221a
DealerDEAL <112918405+DealerDEAL@users.noreply.github.com>
2023-11-22 17:48:59 +0300

    Update random.h

--- random.h
+++ random.h
@@ -1,5 +1,5 @@
-#include <ctime>
 #include <iostream>
+
 using namespace std;
 
 unsigned long seed = 1;  // это то самое стартовое значение,
@@ -7,5 +7,5 @@ unsigned long seed = 1;  // это то самое стартовое значе
 
 int my_rand() {
   seed = seed * 1103515245 + 12345;  // Линейный конгруэнтный метод
-  return (seed / 65536) % 10;     // Вернуть значение в диапазоне 0-32767
+  return (seed / 65536) % 5;     // Вернуть значение в диапазоне 0-32767
 }
